192b3b72571be7d4e08d3192b55c3d4e49b6bd8c
KimMiruJin <sccs604@naver.com>
2021-06-03 23:07:52 +0900

    6월1주차 스프링 입문 프로젝트 환경설정

---
+++ hanbalSpring/src/main/java/com/hanbal/hanbalSpring/HanbalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hanbal.hanbalSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbalSpringApplication.class, args);
+	}
+
+}
---
+++ hanbalSpring/src/test/java/com/hanbal/hanbalSpring/HanbalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbal.hanbalSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanbalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
