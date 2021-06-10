caca4dfbc5921516bdff12ce77e59e970d5f324b
KimMiruJin <sccs604@naver.com>
2021-06-10 23:14:17 +0900

    6월2주차 스프링 입문 AOP

---
+++ hanbalSpring/src/main/java/com/hanbal/hanbalSpring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package com.hanbal.hanbalSpring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect /* AOP는 무조건 작성! */
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hanbal.hanbalSpring..*(..))") /* package 경로인듯..? */
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+        long start = System.currentTimeMillis();
+        System.out.println("Strat : " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        }finally{
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("End : " + joinPoint.toString() + " " + timeMs + " ms");
+        }
+    }
+
+   }
