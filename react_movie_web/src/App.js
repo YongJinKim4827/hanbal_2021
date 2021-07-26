58ceb0270b799eba96dbc658045353a4a690b712
KimMiruJin <sccs604@naver.com>
2021-07-26 21:27:58 +0900

    Chapter 3 State

--- react_movie_web/src/App.js
+++ react_movie_web/src/App.js
@@ -1,68 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }){
-  return <div>
-    <h2> I Like {name} </h2>
-    <h4>{rating}/5.0</h4>
-    <img src = {picture}/>    
-  </div>
-}
-
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-      rating : 5
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-      rating : 4.9
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-      rating : 4.8
-  },
-  {    
-    id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-      rating : 4.7
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-      rating : 4.6
+class App extends React.Component{
+  //function이 아니기 때문에 Return이 없음
+  //React는 자동적으로 Class Component의 render method를 자동으로 실행함
+  
+  //State를 직접 변경해서는 안됨
+  //setState를 호출할때 마다 react는 새로운 State와 함께 render function을 호출
+  state = { //바꾸고 싶은 data를 넣음
+    isLoading : true,
+    movies : []
   }
-];
 
-//Prop Check
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired
-}
+  componentDidMount(){
+    setTimeout(()=> {
+      this.setState({isLoading : false})
+    }, 6000)
+  }
+  render(){
+    const {isLoading} = this.state;
 
-function App() {
-  return (
-    <div>
-      {foodILike.map( dish => 
-        <Food key ={dish.id} name = {dish.name} picture = {dish.image} ratind = {dish.rating}/>
-      )}
-    </div>
+    return <div>{isLoading ?  "Loading" : "We are Ready"}</div>
 
-  );
+  }
 }
 
 export default App;
